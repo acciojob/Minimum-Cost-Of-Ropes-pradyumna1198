7dbe9cd9361f81f065074647bc76d5a15c1f683d
pradyumna1198 <92784304+pradyumna1198@users.noreply.github.com>
2023-06-12 17:29:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 function calculateMinCost() {
   //your code here
+	var input = document.getElementById("rope-lengths").value;
+
+      // Split the input by comma and convert the values to integers
+      var lengths = input.split(",").map(Number);
+
+      // Sort the array of lengths in non-decreasing order
+      lengths.sort(function(a, b) {
+        return a - b;
+      });
+
+      // Initialize variables
+      var cost = 0;
+      var totalCost = 0;
+
+      // Calculate the minimum cost
+      for (var i = 0; i < lengths.length; i++) {
+        cost = lengths[i] + lengths[i + 1];
+        totalCost += cost;
+
+        // Merge the two ropes into one
+        lengths[i + 1] = cost;
+
+        // Sort the array again after merging ropes
+        lengths.sort(function(a, b) {
+          return a - b;
+        });
+      }
+
+      // Display the minimum cost in the result div
+      document.getElementById("result").innerHTML = "Minimum Cost: " + totalCost;
+    }
   
   
   
